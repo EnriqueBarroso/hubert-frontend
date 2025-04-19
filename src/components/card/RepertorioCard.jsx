423f1aed1eca78cbe60faade0cd086d8232820f9
Enrique <enrique.designer2022@gmail.com>
2025-04-19 09:41:45 +0200

    fix: corrige el archivo scss de repertorio

--- src/components/card/RepertorioCard.jsx
+++ src/components/card/RepertorioCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ImagenObra from "../ImagenObra";
-import "../../scss/layout/_repertorio.scss";
+import "../../scss/layout/_repertorioCard.scss";
 
 export default function RepertorioCard({ titulo, sinopsis, imgURL, anio, directorArtistico, onPerfilClick }) {
   return (
